164719ac260c9f50bf69c995d6ae37b0348350ab
LLLjun <1484509745@qq.com>
2024-12-27 21:44:40 +0800

    update

--- sparse_inference.sh
+++ sparse_inference.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES="1" python sparse_inference.py configs/sample_sparse.py \
   --num-frames 64 --resolution 240p --aspect-ratio 9:16 \
   --prompt-path ./prompts.txt 
 
-python clip_score.py \
+CUDA_VISIBLE_DEVICES="1" python clip_score.py \
   --video-folder ./logs/sparse_samples \
   --text-file ./prompts.txt \
   --output-folder ./clip_results \
